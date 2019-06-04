4dfd254c20983decbf3a910868391e63969f9b8a
Marc <mr.moreaux@gmail.com>
2019-06-04 17:41:15 +0200

    move notebooks and create setup

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(
+    name='Kitchen20',
+    version='0.1',
+    packages=['kitchen20', ],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.md').read())
