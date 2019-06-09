592876d89f9e9751e998e9655afafa88c8ca0bbb
Marc <mr.moreaux@gmail.com>
2019-06-09 17:39:29 +0200

    create a setup from lib

--- setup.py
+++ setup.py
@@ -4,5 +4,7 @@ setup(
     name='Kitchen20',
     version='0.1',
     packages=['kitchen20', ],
+    package_data={'': ['config.ini']},
+    include_package_data=True,
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.md').read())
